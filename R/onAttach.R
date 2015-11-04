99cba33addcf61a7a88f1fac59833a7028d09f34
jbedia <joaquin.bedia@gmail.com>
2015-11-04 14:56:47 +0100

    Initial commit

---
+++ R/onAttach.R
@@ -0,0 +1,28 @@
+#' @importFrom utils packageDescription
+#' @importFrom RCurl getURL
+
+.onAttach <- function(...) {
+      pkgname <- "loadeR"
+      lib <- system.file(package = pkgname)
+      ver <- packageDescription(pkgname)$Version
+      builddate <- packageDescription(pkgname)$Date
+      mess <- paste(pkgname, " version ", ver, " (", builddate,") is loaded", sep = "")
+      packageStartupMessage(mess)
+      url <- "https://raw.githubusercontent.com/SantanderMetGroup/loadeR/master/DESCRIPTION"
+      con <- tryCatch(getURL(url, ssl.verifypeer = FALSE), error = function(er) {
+            er <- NULL
+            return(er)
+      })
+      if (!is.null(con)) {
+            b <- readLines(textConnection(con))
+            latest.ver <- package_version(gsub("Version: ", "", b[grep("Version", b)]))
+            if (ver < latest.ver) {
+                  ver.mess1 <- paste("WARNING: Your current version of ", pkgname, " (v", ver, ") is not up-to-date", sep = "")
+                  ver.mess <- paste("Get the latest stable version (", latest.ver, ") using <devtools::install_github('SantanderMetGroup/loadeR'>)", sep = "")
+                  packageStartupMessage(ver.mess1)
+                  packageStartupMessage(ver.mess)
+            }
+      }
+}
+# End
+
