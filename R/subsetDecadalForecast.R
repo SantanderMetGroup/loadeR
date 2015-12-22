31165708c69c72745df76969e818ef74dc9e3c84
miturbide <maibide@gmail.com>
2015-12-22 16:17:25 +0100

    bug fix in subsetDecadalForecast

--- R/subsetDecadalForecast.R
+++ R/subsetDecadalForecast.R
@@ -3,7 +3,7 @@
 #' @description Subset decadal forecast by initialization years
 #' 
 #' @param data field obtained from the output of function loadDecadalForecast. 
-#' @param ly lead years. A vector with integers from 1 to 10. 
+#' @param ly integer indicating the lead year of initialization 
 #' @author J. Bedia, S. Herrera, M. Iturbide, J.M. Gutierrez 
 #' @export
 #' @examples \dontrun{
@@ -15,23 +15,25 @@
 #' tasDECA <- loadDecadalForecast(dataset = "http://www.meteo.unican.es/tds5/dodsC/specs/gfdl_specs_decadal.ncml", 
 #'    latLim = latLim, lonLim = lonLim,
 #'    var = "tas", dictionary = F, years = 1981:1982, season = season)
-#' subsetDecadalForecast(tasDECA, ly = c(2,3,5))
+#' # data corresponding to the initialitation of the year before
+#' subsetDecadalForecast(tasDECA, ly = 1)
 #' }
 
 subsetDecadalForecast <- function(data, ly = c(1, 2, 3)){
       
       init <- data$InitializationDates      
-      
+      dates <- data$Dates$start
       y <- getYearsAsINDEX(data)
       yu <- unique(y)
       
+      yl <- (length(data$InitializationDates)-length(unique(y))+1) - ly
       
       months <- (which(y[1:12] != y[1])[1]-1)/2
       
       if (is.na(months) & length(yu)>1){
             months <- 12
       }else if (is.na(months) & length(yu)==1){
-            months <- length(y)/10
+            months <- length(y)/(length(data$InitializationDates)-length(unique(y))+1)
       }
       
       IND <- list()
@@ -39,14 +41,14 @@ subsetDecadalForecast <- function(data, ly = c(1, 2, 3)){
       for (i in 1:length(yu)){
             year <- which(y == yu[i])
             
-            a <- ly*months
+            a <- yl*months
             ind <- lapply(1:length(a), function(x){
                   (a[x]-months+1):a[x]
                   
             })
             
             yinit <- as.numeric(substr(init, 1, 4))
-            initdates <- yu[i]-10+ly
+            initdates <- yu[i]-(length(data$InitializationDates)-length(unique(y))+1)+yl
             initdates2 <- numeric()
                   for (n in 1:length(initdates)){
                         initdates2[n] <- which(yinit == initdates[n])
@@ -59,7 +61,7 @@ subsetDecadalForecast <- function(data, ly = c(1, 2, 3)){
       
       ind <- sort(unlist(IND))
       ind.init <- sort(unique(unlist(yinit.sub)))
-      
+      if (length(ind.init ) == 0) {stop("ly (lead years) outside the extent of the number of initializations")}
       data$InitializationDates <- init[ind.init]
       data$Dates$start <- dates[ind]
       data$Dates$end <- dates[ind]
