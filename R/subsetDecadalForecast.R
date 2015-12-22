d6ee98bb1827bd99b46bfe0ab05c9055394da694
miturbide <maibide@gmail.com>
2015-12-22 20:21:29 +0100

    minor change

--- R/subsetDecadalForecast.R
+++ R/subsetDecadalForecast.R
@@ -77,3 +77,63 @@ subsetDecadalForecast <- function(data, ly = 1){
       return(data)
 }
 #End
+
+
+
+#' @title Get years as a factor
+#' @description Extract the year as a factor (e.g. for computing annual statistics)
+#' @param obj Any object extending the station or field classes
+#' @return A vector of years of the same length as the time dimension of the object, 
+#' seasonally-adjusted in the case of year-crossing seasons (e.g. DJF). See details.
+#' @details The function performs a very basic operation, extracting the year element from the 
+#' dates previously converted to POSIXlt. The trick lies in the year-crossing seasons. For instance:
+#'  by convention, winter 2001 encompasses December 2000 and January, February 2001. Therefore, in order to compute
+#' annual statistics for a year-crossing season, it is necessary to modify first the vector of years, 
+#' and assign year 2001 to the preceding December. Similarly, the next December 2001 belongs to winter 2002,
+#'  and so on... The function is useful for computing and/or plotting annual statistics, seasonal climatologies ... 
+#' @section Warning:
+#' The function should no be used to extract the actual years vector
+#' @author J. Bedia \email{joaquin.bedia@@gmail.com}
+
+
+getYearsAsINDEX <- function(obj) {
+      season <- getSeason(obj)
+      dimNames <- attr(obj$Data, "dimensions")
+      if (any(grepl("var", dimNames))) {
+            aux.dates <- as.POSIXlt(obj$Dates[[1]]$start)
+      } else {
+            aux.dates <- as.POSIXlt(obj$Dates$start)
+      }
+      yrs <- aux.dates$year + 1900
+      if (!identical(season, sort(season))) {
+            yy <- unique(yrs)[-1]
+            aux <- match(aux.dates$mon + 1, season)
+            brks <- c(1, which(diff(aux) < 0) + 1, length(aux) + 1)
+            l <- lapply(1:(length(brks) - 1), function(x) {
+                  a <- yrs[brks[x] : (brks[x + 1] - 1)]
+                  return(rep(yy[x], length(a)))
+            })
+            yrs  <- do.call("c", l)
+      }
+      return(yrs)
+}
+# End
+
+
+#' @title Get season from a station or field object
+#' @description Retrieves the season encompassed by a station or field object
+#' @param obj Any object extending the station or field classes
+#' @return An integer vector with the season
+#' @author J. Bedia \email{joaquin.bedia@@gmail.com}
+
+
+getSeason <- function(obj) {
+      dimNames <- attr(obj$Data, "dimensions")
+      aux <- if (any(grepl("var", dimNames))) {
+            as.POSIXlt(obj$Dates[[1]]$start)$mon + 1      
+      } else {
+            as.POSIXlt(obj$Dates$start)$mon + 1      
+      }
+      return(unique(aux))
+}
+# End
