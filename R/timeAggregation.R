88fd818af7ee6cefd477e583ba8d22f8ca55632e
miturbide <maibide@gmail.com>
2016-01-23 20:32:38 +0100

    add function getCoordinates (internal)

--- R/timeAggregation.R
+++ R/timeAggregation.R
@@ -325,3 +325,25 @@ annualAggregationMain <- function(data, FUN = c("mean", "min", "max", "sum"), id
 }
 
 #end    
+
+
+
+#' @title Get geographical coordinates of a climate data object
+#' @description Returns the coordinates of a climate data object, either stations
+#'  or field
+#' @param obj Any object extending the station or field classes
+#' @return A list with x and y components
+#' @author J. Bedia \email{joaquin.bedia@@gmail.com}
+#' 
+
+getCoordinates <- function(obj) {
+      if ("station" %in% attr(obj$Data, "dimensions")) {
+            x <- obj$xyCoords[ ,1]
+            y <- obj$xyCoords[ ,2]
+      } else {
+            x <- obj$xyCoords$x
+            y <- obj$xyCoords$y
+      }
+      return(list("x" = x, "y" = y))
+}
+# End
