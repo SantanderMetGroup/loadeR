369df86157e402a76423ed72b565d614c3019280
Your Name <maibide@gmail.com>
2018-08-15 14:23:03 +0200

    add argument pattern to UDG.datasets()

--- R/datasets.R
+++ R/datasets.R
@@ -1,5 +1,6 @@
 #' @title Show UDG datasets
 #' @description Access the installed user's vocabulary
+#' @param pattern Optional. Pattern of the dataset name as passed to function grep.
 #' @return The datasets table, in the form of a data.frame
 #' @note The function assumes that the user has read permission to the package installation directory
 #' @author M Iturbide
@@ -9,8 +10,11 @@
 #' # Default built-in datasets
 #' str(UDG.datasets())
 #' (sets <- UDG.datasets())
-#' sets[grep("^ta", sets$name), ]
+#' sets[grep("CORDEX-EUR44.*historical", sets$name), ]
+#' # Using argument pattern
+#' UDG.datasets(pattern = "CORDEX-EUR44.*historical")$name
 
-UDG.datasets <- function() {
-      read.csv(file.path(find.package("loadeR"), "datasets.txt"), stringsAsFactors = FALSE)[ ,1:3]
+UDG.datasets <- function(pattern = "") {
+      df <- read.csv(file.path(find.package("loadeR"), "datasets.txt"), stringsAsFactors = FALSE)[ ,1:3]
+      df[grep(pattern, df$name),]
 }
