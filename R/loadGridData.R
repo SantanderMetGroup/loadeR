2ddbfa8ecdd80f78524953f07d3e3c893ed8ec51
Your Name <maibide@gmail.com>
2019-07-11 09:40:24 +0200

    bug fix related to vertical variable requests

--- R/loadGridData.R
+++ R/loadGridData.R
@@ -184,7 +184,7 @@ loadGridData <- function(dataset,
       # Dictionary lookup -------------
       # For datasets where level variables exist but are not handled as an extra dimension (e.g. ERA_Interim):
       aux.var <- if (!is.null(aux.level$level)) paste0(aux.level$var, "@", aux.level$level, collapse = "")
-      cd <- check.dictionary(dataset, aux.var, dictionary, time)
+      cd <- tryCatch({check.dictionary(dataset, aux.var, dictionary, time)}, error = function(err) {NULL})
       if (is.null(cd[["shortName"]])) cd <- check.dictionary(dataset, var, dictionary, time)
       # ----------
       shortName <- cd$shortName
