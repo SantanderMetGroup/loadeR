b2f1a51b9350018c7baae28667c3cc546d3ff0a0
jbedia <joaquin.bedia@gmail.com>
2020-06-15 21:03:06 +0200

    Include 2D coordinates

--- R/dataInventory.NetCDF.R
+++ R/dataInventory.NetCDF.R
@@ -12,41 +12,80 @@
 
 
 dataInventory.NetCDF <- function(dataset) {
-    gds <- J("ucar.nc2.dt.grid.GridDataset")$open(dataset)
-    varNames <- unlist(strsplit(gsub("\\[|]|\\s", "", gds$getGrids()$toString()), ","))
-    rm.ind <- grep("^lon|^lat", varNames)  
-    if (length(rm.ind) > 0) {
-        varNames <- varNames[-rm.ind]
-    }
-    if (length(varNames) == 0) {
-        stop("No variables found", call. = FALSE)
-    } else {
-        var.list <- vector(mode = "list", length = length(varNames))   
-        for (i in 1:length(varNames)) {
-            message("[", Sys.time(), "] Retrieving info for \'", varNames[i], "\' (", length(varNames) - i, " vars remaining)")
-            datavar <- gds$getDataVariable(varNames[i])
-            description <- datavar$getDescription()
-            varName <- datavar$getShortName()
-            version <- tryCatch({trimws(datavar$findAttribute("version")$getValues()$toString())}, error = function(e){NA})
-            dataType <- datavar$getDataType()$toString()
-            element.size <- datavar$getDataType()$getSize()
-            data.number <- datavar$getSize()
-            dataSize <- element.size * data.number * 1e-06
-            units <- datavar$getUnitsString()
-            shape <- datavar$getShape()
-            grid <- gds$findGridByShortName(varName)
-            dim.list <- scanVarDimensions(grid)
-            var.list[[i]] <- list("Description" = description,
-                                  "DataType" = dataType,
-                                  "Shape" = shape,
-                                  "Units" = units,
-                                  "DataSizeMb" = dataSize,
-                                  "Version" = version,
-                                  "Dimensions" = dim.list)
-        }
-        names(var.list) <- varNames
-    }
-    gds$close()
-    return(var.list)
+   gds <- J("ucar.nc2.dt.grid.GridDataset")$open(dataset)
+   nc.dataset <- gds$getNetcdfDataset()
+   varNames <- unlist(strsplit(gsub("\\[|]|\\s", "", gds$getGrids()$toString()), ","))
+   rm.ind <- grep("^lon|^lat", varNames)  
+   if (length(rm.ind) > 0) {
+      varNames <- varNames[-rm.ind]
+   }
+   if (length(varNames) == 0) {
+      stop("No variables found", call. = FALSE)
+   } else {
+      var.list <- vector(mode = "list", length = length(varNames))   
+      for (i in 1:length(varNames)) {
+         message("[", Sys.time(), "] Retrieving info for \'", varNames[i], "\' (", length(varNames) - i, " vars remaining)")
+         datavar <- gds$getDataVariable(varNames[i])
+         description <- datavar$getDescription()
+         varName <- datavar$getShortName()
+         version <- tryCatch({trimws(datavar$findAttribute("version")$getValues()$toString())}, error = function(e){NA})
+         dataType <- datavar$getDataType()$toString()
+         element.size <- datavar$getDataType()$getSize()
+         data.number <- datavar$getSize()
+         dataSize <- element.size * data.number * 1e-06
+         units <- datavar$getUnitsString()
+         shape <- datavar$getShape()
+         grid <- gds$findGridByShortName(varName)
+         dim.list <- scanVarDimensions(grid)
+         if (grid$getXDimensionIndex() >= 0) {
+           xDimIndex <- grid$getXDimensionIndex() + 1
+           if (length(dim.list[[xDimIndex]]$Shape) > 1){
+             dimX.list <- list()
+             length(dimX.list) <- length(dim.list[[xDimIndex]]$Shape)
+             for (v in c(1:length(dimX.list))){
+               Xcoord <- nc.dataset$findVariable(dim.list[[xDimIndex]]$Coordinates[v])
+               Xunits <- Xcoord$getUnitsString()
+               XType <- Xcoord$getAxisType()$toString()
+               XAxisShape <- Xcoord$getShape()
+               values <- Xcoord$getCoordValues()
+               aux.XAxis <- Xcoord$getDimensionsString()
+               dimX.list[[v]] <- list("Type" = XType, "Units" = Xunits,
+                                              "Values" = values, "Shape" =  XAxisShape,"Coordinates" = aux.XAxis)
+               names(dimX.list)[v] <- Xcoord$getShortName() # "lon"
+             }
+             dim.list[[xDimIndex]]$Dimensions <- dimX.list
+           }
+         }
+         if (grid$getYDimensionIndex() >= 0) {
+           yDimIndex <- grid$getYDimensionIndex() + 1
+           if (length(dim.list[[yDimIndex]]$Shape) > 1){
+             dimX.list <- list()
+             length(dimX.list) <- length(dim.list[[yDimIndex]]$Shape)
+             for (v in c(1:length(dimX.list))){
+               Xcoord <- nc.dataset$findVariable(dim.list[[yDimIndex]]$Coordinates[v])
+               Xunits <- Xcoord$getUnitsString()
+               XType <- Xcoord$getAxisType()$toString()
+               XAxisShape <- Xcoord$getShape()
+               values <- Xcoord$getCoordValues()
+               aux.XAxis <- Xcoord$getDimensionsString()
+               dimX.list[[v]] <- list("Type" = XType, "Units" = Xunits,
+                                      "Values" = values, "Shape" =  XAxisShape,"Coordinates" = aux.XAxis)
+               names(dimX.list)[v] <- Xcoord$getShortName() # "lon"
+             }
+             dim.list[[yDimIndex]]$Dimensions <- dimX.list
+           }
+         }
+         var.list[[i]] <- list("Description" = description,
+                               "DataType" = dataType,
+                               "Shape" = shape,
+                               "Units" = units,
+                               "DataSizeMb" = dataSize,
+                               "Version" = version,
+                               "Dimensions" = dim.list)
+      }
+      names(var.list) <- varNames
+   }
+   gds$close()
+   return(var.list)
 }
 # End
