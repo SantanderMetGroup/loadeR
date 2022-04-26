b76c51c160ed385f35175aa299271c6ed169b0b9
Ezequiel Cimadevilla Alvarez <ezequiel.cimadevilla@unican.es>
2022-04-26 13:17:54 +0200

    Update login.R

--- R/login.R
+++ R/login.R
@@ -1,6 +1,6 @@
 login <- function(host, username, password) {
     message("[",Sys.time(), "] Setting credentials...")
     J("ucar.httpservices.HTTPFactory")$setCredentials(host, username, password)
-    message("[",Sys.time(), "] Credentials set for user ", username, "@",host ".")
+    message("[",Sys.time(), "] Credentials set for user ", username, "@",host, ".")
     message("[",Sys.time(), "] Please, note that invalid credentials will generate unauthorized errors when accessing the data.")
 }
