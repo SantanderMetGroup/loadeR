a61e284028428d5afb1ee800ebf38238ab95ff97
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-30 15:45:37 +0800

    Delete man-roxygen directory

--- man-roxygen/templateParamMembers.R
+++
@@ -1,4 +0,0 @@
-#' @param members A vector of integers indicating the members to be loaded.
-#'  Default to \code{NULL}, which loads all available members . For instance, \code{members=1:5} will retrieve the first five members of dataset.
-#'   Discontinuous member selections (e.g. \code{members=c(1,5,7)}) are allowed. If the requested dataset has no Ensemble Axis 
-#'    or the requested variable is static (e.g. orography), the argument will be ignored.
